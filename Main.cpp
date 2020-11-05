230176d9e19591108135650b9aae0fbbc6321864
James Puleo <electricman226@gmail.com>
2020-11-05 05:42:29 -0500

    Riverbend: Catch unhandled exceptions, and ABORT_F them with loguru

--- Main.cpp
+++ Main.cpp
@@ -24,18 +24,29 @@
 
 int main()
 {
-    loguru::g_preamble_uptime = false;
-    loguru::g_preamble_thread = false;
-    loguru::add_file("riverbend.log", loguru::FileMode::Append, loguru::Verbosity_INFO);
-
-    loguru::set_fatal_handler([](const loguru::Message& msg)
+#ifndef _DEBUG
+    try
     {
-        NativeMessageBox::open("Fatal Error", msg.message, NativeMessageBox::Type::Error, NativeMessageBox::Button::OK);
-    });
+#endif // _DEBUG
+        loguru::g_preamble_uptime = false;
+        loguru::g_preamble_thread = false;
+        loguru::add_file("riverbend.log", loguru::FileMode::Append, loguru::Verbosity_INFO);
 
-    CHECK_F(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == 0, "SDL failed to initialize: %s", SDL_GetError());
+        loguru::set_fatal_handler([](const loguru::Message& msg)
+        {
+            NativeMessageBox::open("Fatal Error", msg.message, NativeMessageBox::Type::Error, NativeMessageBox::Button::OK);
+        });
 
-    Application app;
+        CHECK_F(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == 0, "SDL failed to initialize: %s", SDL_GetError());
 
-    return app.start();
+        Application app;
+
+        return app.start();
+#ifndef _DEBUG
+    }
+    catch(const std::exception& e)
+    {
+        ABORT_F("Unhandled exception: %s", e.what());
+    }
+#endif // _DEBUG
 }
